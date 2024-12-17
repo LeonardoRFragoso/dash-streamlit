8f3f6839f90092ae393001da964fc7c7d20b8aa6
Leonardo Fragoso <leonardorfragoso@gmail.com>
2024-12-17 11:13:44 -0300

    Ajustando visualização dos filtros

--- run.py
+++ run.py
@@ -144,7 +144,51 @@ def save_cache(cache):
     with open(CACHE_FILE, 'w') as f:
         json.dump(cache, f)
 
-# Função para carregar os dados do Google Drive
+def get_cached_coordinates(local, api_key, cache):
+    """Get coordinates from cache or API."""
+    if local in cache:
+        return cache[local]
+    lat, lng = get_coordinates(local, api_key)
+    if lat is not None and lng is not None:
+        cache[local] = (lat, lng)
+    return lat, lng
+
+def get_coordinates(local, api_key):
+    """Fetch coordinates for a given location using OpenCage API."""
+    url = f'https://api.opencagedata.com/geocode/v1/json?q={local}&key={api_key}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data['results']:
+            geometry = data['results'][0]['geometry']
+            return geometry['lat'], geometry['lng']
+    return None, None
+
+def autenticar_google_drive():
+    """Autentica no Google Drive usando credenciais de serviço."""
+    # Converte explicitamente o conteúdo de CREDENTIALS para string e depois para JSON
+    credentials_str = str(st.secrets["CREDENTIALS"])  # Converte AttrDict para string
+    credentials_dict = json.loads(credentials_str.replace("\n", "\\n"))  # Ajusta quebras de linha e carrega JSON
+    
+    # Cria as credenciais a partir do dicionário
+    credentials = Credentials.from_service_account_info(
+        credentials_dict, 
+        scopes=["https://www.googleapis.com/auth/drive"]
+    )
+    
+    # Retorna o serviço autenticado do Google Drive
+    return build("drive", "v3", credentials=credentials)
+
+def obter_id_ultima_planilha():
+    """Obtém o ID da última planilha salva no JSON."""
+    try:
+        with open(st.secrets["ULTIMA_PLANILHA_JSON"], 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            return data.get("file_id")
+    except Exception as e:
+        st.error(f"Erro ao carregar o ID da última planilha: {e}")
+        st.stop()
+
 def carregar_dados_google_drive():
     """Carrega os dados da última planilha no Google Drive."""
     try:
