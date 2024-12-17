222866acdc9e4bae35feb6aa65791cbe590d2fce
Leonardo Fragoso <leonardorfragoso@gmail.com>
2024-12-17 16:17:04 -0300

    Ajustando visualização dos filtros

--- run.py
+++ run.py
@@ -150,6 +150,13 @@ filtered_data = data_cleaned[
     (data_cleaned['Dia da Consulta'] <= pd.Timestamp(end_date))
 ]
 
+# Função auxiliar para tratar valores monetários
+def safe_float(value):
+    try:
+        return float(str(value).replace(",", ".").replace("R$", "").strip())
+    except (ValueError, TypeError):
+        return 0.00
+
 # Gráficos
 st.markdown("<h2 class='titulo-centralizado'>Top 10 Veículos com Mais Multas e Valores Totais</h2>", unsafe_allow_html=True)
 st.plotly_chart(create_vehicle_fines_chart(filtered_data), use_container_width=True)
