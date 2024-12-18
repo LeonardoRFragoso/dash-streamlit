c245c02459844b45695874ade5d66202f3da8c93
Leonardo Fragoso <leonardorfragoso@gmail.com>
2024-12-18 07:37:18 -0300

    Ajustando tabelas no streamlit

--- data_processing.py
+++ data_processing.py
@@ -1,43 +1,26 @@
 import pandas as pd
 
-def load_data(file_path, sheet_name='Multas'):
+def carregar_e_limpar_dados(carregar_dados_func):
     """
     Carrega e faz o pré-processamento básico dos dados.
     """
     try:
-        # Carrega a planilha
-        df = pd.read_excel(file_path, sheet_name=sheet_name)
-
-        # Converte colunas de datas para o formato datetime
+        df = carregar_dados_func()
         df['Dia da Consulta'] = pd.to_datetime(df['Dia da Consulta'], errors='coerce', dayfirst=True)
         df['Data da Infração'] = pd.to_datetime(df['Data da Infração'], errors='coerce', dayfirst=True)
 
-        # Verificar se as colunas essenciais existem
-        required_columns = ['Dia da Consulta', 'Data da Infração', 'Auto de Infração', 'Valor a Ser Pago (R$)']
-        missing_columns = [col for col in required_columns if col not in df.columns]
-        if missing_columns:
-            raise ValueError(f"As colunas essenciais estão faltando: {', '.join(missing_columns)}")
-
-        # Preprocessa o campo 'Valor a Ser Pago (R$)'
+        # Preprocessar valores
         df['Valor a Ser Pago (R$)'] = df['Valor a Ser Pago (R$)'].astype(str)\
             .str.replace(r'[^\d,.-]', '', regex=True)\
             .str.replace(r'\.(?=\d{3,})', '', regex=True)\
             .str.replace(',', '.')\
             .apply(lambda x: float(x) if x.replace('.', '', 1).isdigit() else 0)
-
         return df
-
     except Exception as e:
         print(f"Erro ao carregar os dados: {e}")
         raise
 
-def clean_data(df):
-    """
-    Remove duplicados com base na coluna 'Auto de Infração'.
-    """
-    return df.drop_duplicates(subset=['Auto de Infração'])
-
-def verify_columns(df, required_columns):
+def verificar_colunas_essenciais(df, required_columns):
     """
     Verifica se as colunas essenciais estão presentes nos dados.
     """
@@ -45,13 +28,13 @@ def verify_columns(df, required_columns):
     if missing_columns:
         raise ValueError(f"Colunas essenciais ausentes: {', '.join(missing_columns)}")
 
-def preprocess_values(df):
+def preprocessar_valores(df):
     """
-    Converte valores monetários e limpa dados de colunas numéricas específicas.
+    Preprocessa valores monetários no DataFrame.
     """
     if 'Valor a Ser Pago (R$)' in df.columns:
         df['Valor a Ser Pago (R$)'] = df['Valor a Ser Pago (R$)'].astype(str)\
-            .str.replace(r'[^\d,.-]', '', regex=True)\
+            .str.replace(r'[^d,.-]', '', regex=True)\
             .str.replace(r'\.(?=\d{3,})', '', regex=True)\
             .str.replace(',', '.')\
             .apply(lambda x: float(x) if x.replace('.', '', 1).isdigit() else 0)
